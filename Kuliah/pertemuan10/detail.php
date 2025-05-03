19b4262c278135961ad1fbe658ae3b55cf74fbf9
alimustopa4 <ali.baramahesa01@gmail.com>
2025-05-03 07:41:54 +0700

    koneksi dan tampil data

---
+++ Kuliah/pertemuan10/detail.php
@@ -0,0 +1,26 @@
+<?php 
+require "function.php";
+$mahasiswa = query("SELECT * FROM mahasiswa WHERE id = $_GET[id]");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detail Mahasiswa</title>
+</head>
+<body>
+    <h3>Detail Mahasiswa</h3>.
+    <?php foreach($mahasiswa as $mhs) : ?>
+    <ul>
+        <li><?= $mhs["nim"]; ?></li>
+        <li><?= $mhs["nama"]; ?></li>
+        <li><?= $mhs["email"]; ?></li>
+        <li><?= $mhs["jurusan"]; ?></li>
+        <li><a href="">Ubah</a> | <a href="">Hapus</a></li>
+        <li><a href="latihan2.php">Kembali Kehalaman Detail</a></li>
+    </ul>
+    <?php endforeach; ?>
+</body>
+</html>
---
+++ Kuliah/pertemuan10/function.php
@@ -0,0 +1,15 @@
+<?php 
+    // koneksi ke database
+    $koneksi = mysqli_connect("Localhost", "root", "", "zaki");
+
+    function query($query) {
+        global $koneksi;
+        $result = mysqli_query($koneksi, $query);
+        $rows = [];
+
+        while ($row = mysqli_fetch_assoc($result)) {
+            $rows[] = $row;
+        }
+        return $rows;
+    }
+?>
